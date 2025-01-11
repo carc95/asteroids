494ac0b3a6c3024b6d0d001136279074266d588b
carc95 <carriggc@yahoo.co.uk>
2025-01-11 22:59:33 +0000

    J: added a rate limit on the shot method

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     # in the player class
 
@@ -24,6 +25,7 @@ class Player(CircleShape):
         self.rotation += (PLAYER_TURN_SPEED * dt)
 
     def update(self, dt):
+        
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -34,8 +36,14 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-        if keys[pygame.K_SPACE]:
+        if (keys[pygame.K_SPACE]) and (self.timer <= 0):
             self.shoot()
+        
+        if (self.timer > 0):
+            self.timer -= dt
+            
+            
+        
 
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -44,5 +52,6 @@ class Player(CircleShape):
     def shoot(self):
         shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
 
         
