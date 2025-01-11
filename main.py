6ae99bc537f8ec4bdd9f25abda4fd155309334f5
carc95 <carriggc@yahoo.co.uk>
2025-01-11 00:28:04 +0000

    G: using groups to control objects in main.py

--- main.py
+++ main.py
@@ -12,17 +12,23 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        for update in updatable:
+            player.update(dt)
+        for drawing in drawable:
+            player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
